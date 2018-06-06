060d9fa1419d8dfa1fc36275910a605b3f575ac7
António Almeida <theantonioalmeida@gmail.com>
2018-06-06 21:41:15 +0100

    Update compile.sh and add examples folder with 2 examples

--- compile.sh
+++ compile.sh
@@ -8,6 +8,7 @@ cd ..
 javac -cp .:junit-4.12.jar:hamcrest-core-1.3.jar -d bin -sourcepath . src/*/*.java 
 
 cp -r src/testsuite bin
+cp -r src/examples bin
 cp hamcrest-core-1.3.jar bin
 cp junit-4.12.jar bin
 cp io.class bin
