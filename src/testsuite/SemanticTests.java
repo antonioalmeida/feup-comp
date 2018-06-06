20ffc48e4a3397736e07674d52f86d7b111ff435
António Almeida <theantonioalmeida@gmail.com>
2018-06-06 20:39:36 +0100

    Finish semantic tests

--- src/testsuite/SemanticTests.java
+++ src/testsuite/SemanticTests.java
@@ -10,9 +10,9 @@ import static org.junit.Assert.*;
 public class SemanticTests {
 
     @Test
-    public void test1() {
+    public void testIfs1() {
 
-        String testName = "testsuite/semantic_analysis/test1.yal";
+        String testName = "testsuite/semantic_analysis/testIfs1.yal";
 
         FileInputStream testStream = TestUtils.openFile(testName);
 
@@ -32,4 +32,113 @@ public class SemanticTests {
 
         assertEquals(4, nErrors);
     }
+
+    @Test
+    public void testIfs2() {
+
+        String testName = "testsuite/semantic_analysis/testIfs2.yal";
+
+        FileInputStream testStream = TestUtils.openFile(testName);
+
+        int nErrors = Yal.runAndGetErrors(testStream);
+
+        assertEquals(2, nErrors);
+    }
+
+    @Test
+    public void testAssigns() {
+
+        String testName = "testsuite/semantic_analysis/testAssigns.yal";
+
+        FileInputStream testStream = TestUtils.openFile(testName);
+
+        int nErrors = Yal.runAndGetErrors(testStream);
+
+        assertEquals(25, nErrors);
+    }
+
+    @Test
+    public void testGlobalReassign() {
+
+        String testName = "testsuite/semantic_analysis/testGlobalReassign.yal";
+
+        FileInputStream testStream = TestUtils.openFile(testName);
+
+        int nErrors = Yal.runAndGetErrors(testStream);
+
+        assertEquals(5, nErrors);
+    }
+
+    @Test
+    public void testReturnInit() {
+
+        String testName = "testsuite/semantic_analysis/testReturnInit.yal";
+
+        FileInputStream testStream = TestUtils.openFile(testName);
+
+        int nErrors = Yal.runAndGetErrors(testStream);
+
+        assertEquals(1, nErrors);
+    }
+
+    @Test
+    public void testFunctionCall() {
+
+        String testName = "testsuite/semantic_analysis/testFunctionCall.yal";
+
+        FileInputStream testStream = TestUtils.openFile(testName);
+
+        int nErrors = Yal.runAndGetErrors(testStream);
+
+        assertEquals(3, nErrors);
+    }
+
+    @Test
+    public void testIfs3() {
+
+        String testName = "testsuite/semantic_analysis/testIfs3.yal";
+
+        FileInputStream testStream = TestUtils.openFile(testName);
+
+        int nErrors = Yal.runAndGetErrors(testStream);
+
+        assertEquals(3, nErrors);
+    }
+
+    @Test
+    public void testFunctionReturn() {
+
+        String testName = "testsuite/semantic_analysis/testFunctionReturn.yal";
+
+        FileInputStream testStream = TestUtils.openFile(testName);
+
+        int nErrors = Yal.runAndGetErrors(testStream);
+
+        assertEquals(3, nErrors);
+    }
+
+    @Test
+    public void testComparison() {
+
+        String testName = "testsuite/semantic_analysis/testComparison.yal";
+
+        FileInputStream testStream = TestUtils.openFile(testName);
+
+        int nErrors = Yal.runAndGetErrors(testStream);
+
+        assertEquals(1, nErrors);
+    }
+
+    @Test
+    public void testIfs4() {
+
+        String testName = "testsuite/semantic_analysis/testIfs4.yal";
+
+        FileInputStream testStream = TestUtils.openFile(testName);
+
+        int nErrors = Yal.runAndGetErrors(testStream);
+
+        assertEquals(3, nErrors);
+    }
+
 }
